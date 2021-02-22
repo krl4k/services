b6e07e04c27451cb884829383e5acec766b8ae0b
krl4k <fhds.hdd@mail.ru>
2021-02-22 20:56:04 +0300

    add nginx ssh

--- start.sh
+++ start.sh
@@ -3,18 +3,10 @@
 minikube start --vm-driver=virtualbox && \
 minikube addons enable dashboard && \
 minikube addons enable metallb && \
-
-
 eval $(minikube docker-env) && \
-
-kubectl apply -f configmap.yaml && \
-
-
-
-
-docker build -t nginx_image srcs/nginx/. &&\
-kubectl apply -f nginx/nginx.yaml && \
-
+kubectl apply -f srcs/configmap.yaml && \
+docker build -t nginx_image srcs/nginx/. && \
+kubectl apply -f srcs/nginx/srcs/nginx.yaml && \
 minikube dashboard
 
 
