9ce6f6c7771c5beeddbcaa5e028936af9a4c413a
krl4k <fhds.hdd@mail.ru>
2021-02-25 20:28:59 +0300

    added blowfosh code

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -3,6 +3,7 @@ rc default
 /etc/init.d/mariadb setup
 /etc/init.d/mariadb start
 #
+mysql_secure_installation
 mysql -uroot <<MYSQL_SCRIPT
 CREATE DATABASE mydb;
 CREATE USER 'root'@'%' IDENTIFIED BY 'root';
@@ -10,8 +11,9 @@ GRANT ALL PRIVILEGES ON DATABASE* TO 'root';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
+
 #mysql -uroot DB < DB.sql
-#mysql -u root -p
+#mysql -uroot db
 /etc/init.d/mariadb stop
 exec /usr/bin/mysqld_safe
 #/bin/sh
