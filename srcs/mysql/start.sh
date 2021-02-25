7759441e5e3fac2b4b5b3cc1c5072a44980b06ff
krl4k <fhds.hdd@mail.ru>
2021-02-25 18:56:44 +0300

    work php with mysql

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 rc default
-/etc/init.d/mariadb setups
+/etc/init.d/mariadb setup
 /etc/init.d/mariadb start
-
+#
 mysql -uroot <<MYSQL_SCRIPT
-CREATE DATABASE DATABASE;
-CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON DATABASE* TO 'admin';
+CREATE DATABASE mydb;
+CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON DATABASE* TO 'root';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 #mysql -uroot DB < DB.sql
-
+#mysql -u root -p
 /etc/init.d/mariadb stop
 exec /usr/bin/mysqld_safe
+#/bin/sh
--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -32,7 +32,6 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql-svc';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
-$cfg['Servers'][$i]['host'] = 'mysql-svc';
 $cfg['Servers'][$i]['port'] = '3306';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
